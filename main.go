aa2bfc115d81742a337695316f6a5b65c8fc38bb
fadlur <fadloer@gmail.com>
2024-08-18 17:23:45 +0700

    load config file used Viper

--- main.go
+++ main.go
@@ -6,17 +6,23 @@ import (
 
 	"github.com/fadlur/learn_golang_sqlc/api"
 	db "github.com/fadlur/learn_golang_sqlc/db/sqlc"
+	"github.com/fadlur/learn_golang_sqlc/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://postgres:147789@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "127.0.0.1:8080"
-)
+// const (
+// 	dbDriver      = "postgres"
+// 	dbSource      = "postgresql://postgres:147789@localhost:5432/simple_bank?sslmode=disable"
+// 	serverAddress = "127.0.0.1:8080"
+// )
 
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config: ", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
@@ -25,7 +31,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 
 	if err != nil {
 		log.Fatal("can not start server")
